9979c6f47d670debcce2d54e50b78865f281099d
atifmahmud <atifmahmud95@gmail.com>
2019-11-07 14:00:47 -0800

    Added animation for pencil

---
+++ ScholarsVR-Classroom/Assets/Scripts/Pencil.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pencil : MonoBehaviour
+{
+    Rigidbody rb;
+    bool animationBack = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Pencil Script");
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (animationBack)
+       {
+            transform.Rotate(10 * Time.deltaTime, 10 * Time.deltaTime, 10 * Time.deltaTime);
+            animationBack = false;
+       } else
+       {
+            transform.Rotate(-10, -10, -10);
+            animationBack = true;
+        }
+    }
+}
