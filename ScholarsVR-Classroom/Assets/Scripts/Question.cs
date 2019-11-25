48610f3cee40d4ca5a31739d309279a499ba3a75
BrantYin <ryin980818@gmail.com>
2019-11-25 10:25:50 -0800

    Basic functionality for question panel

---
+++ ScholarsVR-Classroom/Assets/Scripts/Question.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : ScriptableObject
+{
+    public string body = "";
+    public string optionA = "";
+    public string optionB = "";
+    public string optionC = "";
+    public string optionD = "";
+    public int correctAnswer = 0;
+
+    public Question(string body, string optionA, string optionB, 
+                    string optionC, string optionD, int correctAnswer)
+    {
+        this.body = body;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.optionD = optionD;
+        this.correctAnswer = correctAnswer;
+    }
+}
---
+++ ScholarsVR-Classroom/Assets/Scripts/QuestionPanel.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionPanel : MonoBehaviour
+{
+    private Question question;
+    private bool answerCorrect;    
+
+    public int score = 0;
+    public Text body;
+    public Text optionA;
+    public Text optionB;
+    public Text optionC;
+    public Text optionD;
+
+    public Button buttonA;
+    public Button buttonB;
+    public Button buttonC;
+    public Button buttonD;
+
+    private void Start()
+    {
+        answerCorrect = false;
+
+        question = 
+            new Question("Hi, this is a question, the third option is correct", 
+                        "Here is the first option", 
+                        "Here is the second option", 
+                        "Here is the third option", 
+                        "Here is the fourth option", 
+                        2);
+        
+        body.text = question.body;
+        optionA.text = question.optionA;
+        optionB.text = question.optionB;
+        optionC.text = question.optionC;
+        optionD.text = question.optionD;
+
+        SetDisabledColor();
+    }
+
+    public void SelectA() {
+        if (!answerCorrect) {
+            if (question.correctAnswer == 0) {
+                answerCorrect = true;
+                DisableButtons();
+                score++;
+            }
+        }
+    }
+
+    public void SelectB() {
+        if (!answerCorrect) {
+            if (question.correctAnswer == 1) {
+                answerCorrect = true;
+                DisableButtons();
+                score++;
+            }
+        }
+    }
+
+    public void SelectC() {
+        if (!answerCorrect) {
+            if (question.correctAnswer == 2) {
+                answerCorrect = true;
+                DisableButtons();
+                score++;
+            }
+        }
+    }
+
+    public void SelectD() {
+        if (!answerCorrect) {
+            if (question.correctAnswer == 3) {
+                answerCorrect = true;
+                DisableButtons();
+                score++;
+            }
+        }
+    }
+
+    private void SetDisabledColor()
+    {   
+        ColorBlock colors = buttonA.colors;
+        colors.disabledColor = question.correctAnswer == 0 ? Color.green : Color.red;    
+        buttonA.colors = colors;
+
+        colors = buttonB.colors;
+        colors.disabledColor = question.correctAnswer == 1 ? Color.green : Color.red;    
+        buttonB.colors = colors; 
+
+        colors = buttonC.colors;
+        colors.disabledColor = question.correctAnswer == 2 ? Color.green : Color.red;    
+        buttonC.colors = colors; 
+
+        colors = buttonD.colors;
+        colors.disabledColor = question.correctAnswer == 3 ? Color.green : Color.red;    
+        buttonD.colors = colors;  
+    }
+
+    private void DisableButtons() 
+    {
+        buttonA.interactable = false;
+        buttonB.interactable = false;
+        buttonC.interactable = false;
+        buttonD.interactable = false;
+    }
+
+    public void resetButtons() 
+    {
+        buttonA.interactable = true;
+        buttonB.interactable = true;
+        buttonC.interactable = true;
+        buttonD.interactable = true;
+    }
+}
