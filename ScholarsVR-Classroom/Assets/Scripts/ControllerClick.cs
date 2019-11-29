3edd3ae370a3e7ddcdd9b1ca4c63b23e8fb4195b
atifmahmud <atifmahmud95@gmail.com>
2019-11-29 14:55:22 -0800

    Controller Click Working

--- ScholarsVR-Classroom/Assets/Scripts/ControllerClick.cs
+++ ScholarsVR-Classroom/Assets/Scripts/ControllerClick.cs
@@ -7,17 +7,16 @@ using UnityEngine;
 public class ControllerClick : MonoBehaviour
 {
 
-    public OVRInput.Controller right_controller; 
+    public OVRInput.Controller right_controller;
     public OVRInput.Controller left_controller;
-    Vector3 right_controller_position;
-    Vector3 left_controller_position;
     bool right_press = false;
     bool left_press = false;
+    public GameObject controller;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -28,58 +27,15 @@ public class ControllerClick : MonoBehaviour
         right_press = OVRInput.Get(OVRInput.RawButton.RIndexTrigger);
         left_press = OVRInput.Get(OVRInput.RawButton.LIndexTrigger);
 
-        right_controller_position = OVRInput.GetLocalControllerPosition(right_controller);
-        left_controller_position = OVRInput.GetLocalControllerPosition(left_controller);
 
         RaycastHit hit;
 
-        if (Physics.Raycast(right_controller_position, right_controller_position + Vector3.forward, out hit, Mathf.Infinity))
+        if (Physics.Raycast(controller.transform.position, controller.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
         {
             if (hit.collider.CompareTag("Prof"))
             {
-                // if (right_press)
+                //if (right_press)
                     Debug.Log("Right Click on Prof");
-            }
-        }
-        
-        /*
-        if (right_press)
-        {
-            RightClickHandler();
-        }
-
-        if (left_press)
-        {
-            LeftClickHandler();
-        }
-        */
-    }
-
-    void RightClickHandler()
-    {
-        right_press = false;
-        Debug.Log("Right Press");
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
-        {
-            if (hit.collider.CompareTag("Prof"))
-            {
-                
-                Debug.Log("Right Click on Prof");
-            }
-        }
-    }
-
-    void LeftClickHandler()
-    {
-        left_press = false;
-        Debug.Log("LeftPress");
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
-        {
-            if (hit.collider.CompareTag("Prof"))
-            {
-                Debug.Log("Left Click on Prof");
             }
         }
     }
