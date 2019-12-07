0dfeb8961c07968f265e01f6e8c929e5dad9be65
atifmahmud <atifmahmud95@gmail.com>
2019-12-06 18:07:37 -0800

    Ready

--- ScholarsVR-Classroom/Assets/Scripts/Prof.cs
+++ ScholarsVR-Classroom/Assets/Scripts/Prof.cs
@@ -8,6 +8,7 @@ public class Prof : MonoBehaviour
     private static Renderer renderer;
     private static Shader highlightShader;
     private static Renderer profRenderer;
+    private static AudioSource audio;
     private static bool highlighted = false;
 
     // Start is called before the first frame update
@@ -16,11 +17,17 @@ public class Prof : MonoBehaviour
         renderer = gameObject.GetComponent<Renderer>();
         profRenderer = gameObject.GetComponent<Renderer>();
         highlightShader = Shader.Find("Highlight");
+        audio = gameObject.GetComponent<AudioSource>();
 }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public static bool IsPlaying()
+    {
+        return audio.isPlaying;
     }
 }
